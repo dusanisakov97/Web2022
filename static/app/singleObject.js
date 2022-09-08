ae5a0b3848af0a01224063df301048ecb295c90d
dusanisakov <dusisakov@gmail.com>
2022-09-08 22:42:00 +0200

    one object

--- static/app/singleObject.js
+++ static/app/singleObject.js
@@ -1,19 +1,59 @@
 Vue.component("single-object", {
 	template: `
-		<div class="ui container">
-			<div class="">
+		<div class="ui container" v-if="sportsObject != null">
+			<div class="ui grid">
+				<div class="four wide column">
+					<div class="card">
+						<div class="image">
+							<img :src="sportsObject.image">
+						</div>
+						<div class="content">
+							<div class="header">{{sportsObject.name}}</div>
+							<div class="meta">
+							<a>{{sportsObject.type}}</a>
+							</div>
+							<div class="description">
+							Location: {{sportsObject.location.street + ', ' + sportsObject.location.city + ', ' + sportsObject.location.zipCode}}
+							</div>
+						</div>
+						<div class="extra content">
+							<span class="right floated">
+							<i class="check icon" v-if="sportsObject.working"></i>
+							<i class="close	 icon" v-if="!sportsObject.working"></i>
 
+							</span>
+							<span>
+							<i class="star icon"></i>
+							{{sportsObject.averageMark}}
+							</span>
+						</div>
+						<div class="extra content">
+							<span class="right floated">
+							<i class="time icon"></i>
 
-			</div>
+							{{sportsObject.endWorking}}
+							</span>
+							<span>
+							<i class="time icon"></i>
+							{{sportsObject.startWorking}}
+							</span>
+						</div>
+					</div>
+				
+				</div>
+				<div class="twelve wide column"></div>
+				
+	 		</div>
 		</div>
 		
 	`,
 	data: function() {
 		return {
-			id : "",
+			id : this.$route.params.id,
+			sportsObject: null
 		}
 	}, 
-	mounted: function() {
+	beforeCreate: function() {
 		this.id = this.$route.params.id;
 		axios.get("/sports-object", { params: {
 			id: this.id
